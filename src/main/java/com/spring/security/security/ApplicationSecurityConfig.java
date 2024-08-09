a5d0098323753b56de89e055704cd12e9d01cf64
coding-jana <jana63sc@gmail.com>
2024-08-09 16:19:57 +0200

    added Users and their roles/permissions

--- src/main/java/com/spring/security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/spring/security/security/ApplicationSecurityConfig.java
@@ -1,11 +1,19 @@
 package com.spring.security.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static com.spring.security.security.ApplicationUserRole.ADMIN;
+import static com.spring.security.security.ApplicationUserRole.STUDENT;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 
@@ -13,15 +21,44 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class ApplicationSecurityConfig{
 
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
-                .authorizeRequests(authorizeRequests ->
+                .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
-                                .requestMatchers("/","index","/css/*", "/js/*").permitAll()
+                                .requestMatchers("/", "index", "/css/**", "/js/**").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .httpBasic(withDefaults());
         return http.build();
+    }
+
+
+    @Bean
+    protected UserDetailsService userDetailsService(){
+        UserDetails annaSmithUser = User.builder()
+                .username("annasmith")
+                .password(passwordEncoder.encode("password"))
+                .roles(STUDENT.name())
+                .build();
+
+        UserDetails lindaUser = User.builder()
+                .username("linda")
+                .password(passwordEncoder.encode("password123"))
+                .roles(ADMIN.name())
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                annaSmithUser,
+                lindaUser
+        );
+
     }
 }
